63b5c896bfdb5c96bdf2293460ff18ba2be80f7c
sefatanam <sefatanam@gmail.com>
2022-03-24 00:18:10 +0600

    mongodb cloud credentials added

--- environments/development.js
+++ environments/development.js
@@ -9,9 +9,17 @@ class Credentials
 }
 
 exports.config = {
-    connectionString: `mongodb://${Credentials.username}:${Credentials.password}@${Credentials.host}:${Credentials.port}?retryWrites=true&w=majority`,
+    // connectionString: `mongodb://${Credentials.username}:${Credentials.password}@${Credentials.host}:${Credentials.port}?retryWrites=true&w=majority`,
+    connectionString: "mongodb+srv://cnq:rPKMMHEtEVZI3TTb@cluster0.audg9.mongodb.net?retryWrites=true&w=majority",
     useNewUrlParser: true,
     databaseName: Credentials.databaseName,
     useUnifiedTopology: true,
-    winstonConnectionString: `mongodb://${Credentials.username}:${Credentials.password}@${Credentials.host}:${Credentials.port}/${Credentials.databaseName}?retryWrites=true&w=majority&authSource=${Credentials.authSource}`
+    // winstonConnectionString: `mongodb://${Credentials.username}:${Credentials.password}@${Credentials.host}:${Credentials.port}/${Credentials.databaseName}?retryWrites=true&w=majority&authSource=${Credentials.authSource}`,
+    winstonConnectionString: `mongodb+srv://cnq:rPKMMHEtEVZI3TTb@cluster0.audg9.mongodb.net/University?retryWrites=true&w=majority&authSource=${Credentials.authSource}`,
+
 }
+
+
+
+
+// const uri = "mongodb+srv://cnq:<password>@cluster0.audg9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
